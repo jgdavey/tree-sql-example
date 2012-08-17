5a41ae93527b904083cae73d59d4a8f102ed9400
Joshua Davey <josh@joshuadavey.com>
2012-08-17 14:48:45 -0500

    Basic Rails application

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source :rubygems
+
+gem 'rails', '3.2.8'
+gem 'pg'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'fabrication'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tree::Application.config.secret_token = '223ca724d1f7a52ffa8e64bd7c13db303627ba79c143c7f9fac34c17b543e0defb275db95f56188159a1abfe0619db3cd0014c0f894c6a3acc291cdab146ab54'
