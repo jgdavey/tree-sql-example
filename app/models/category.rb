18939ccd3c578cd8df655f890fe536450bfa349d
Joshua Davey <josh@joshuadavey.com>
2012-08-19 21:42:16 -0500

    Raw SQL implementation

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,6 +10,23 @@ class Category < ActiveRecord::Base
   end
 
   def self_and_descendents
-    [self] + children.map(&:self_and_descendents).flatten
+    self.class.tree_for(self)
+  end
+
+  def self.tree_for(instance)
+    find_by_sql <<-SQL
+      WITH RECURSIVE search_tree(id, path) AS (
+          SELECT id, ARRAY[id]
+          FROM #{table_name}
+          WHERE id = #{instance.id}
+        UNION ALL
+          SELECT #{table_name}.id, path || #{table_name}.id
+          FROM search_tree
+          JOIN #{table_name} ON #{table_name}.parent_id=search_tree.id
+          WHERE NOT #{table_name}.id = ANY(path)
+      )
+      SELECT * FROM #{table_name}
+      WHERE id IN (SELECT id FROM search_tree);
+    SQL
   end
 end
